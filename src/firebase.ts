697a17df15d567c777255be18673903d0b3c4c41
alfurqanherbsclinic-lab <alfurqanherbsclinic@gmail.com>
2026-07-23 11:28:41 +0300

    Add files via upload

--- src/firebase.ts
+++ src/firebase.ts
@@ -1,6 +1,6 @@
 import { initializeApp, getApps, getApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
-import firebaseConfig from "../firebase-applet-config.json";
+import firebaseConfig from "../../firebase-applet-config.json";
 
 const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
 
